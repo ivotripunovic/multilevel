8509423376e9975de9d8d5bd1f12a008939fd5f2
Ivo Tripunovic <tripunovic@yahoo.com>
2025-11-28 13:59:59 +0100

    fixed unused imports

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,11 +1,7 @@
-import os
 from decimal import Decimal
-from django.conf import settings
-from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseForbidden
-from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
+from django.shortcuts import get_object_or_404, redirect
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
